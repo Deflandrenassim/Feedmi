d17f0b6a33f32f357b0a4c3b2ab198c5ee213aec
Deflandrenassim <nassim.def@gmail.com>
2022-01-21 09:05:23 +0100

    correction bug css header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+      
     <div class="container">
+
+      <div class="row">
       <img
         src="@/assets/images/feedmi-logo.png"
         class="logo-feedmi"
         alt="Logo de feedmi"
       />
-      <div class="nav">
-        <router-link to="/" class="choix"> Accueil </router-link>
-        <router-link to="/about" class="choix"> Qui somme nous </router-link>
-        <router-link to="/Connexion" class="choix"> Connexion </router-link>
+      <nav class="Navbar">
+        <router-link to="/" class="choix"> Accueil </router-link> 
+        <router-link to="/about" class="choix"> Qui somme nous </router-link> 
+       <button> <router-link to="/Connexion" class="choix"> Connexion </router-link> </button>
+      </nav>
+
       </div>
+      
     </div>
     <router-view />
   </div>
@@ -22,11 +28,30 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #27333f;
+  margin : 0px;
 }
-.container{
+.row{
+  display: flex;
+  flex-direction: row;
+  padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
   
 }
-
+.Navbar{
+ 
+    display: flex;
+    align-items: center;
+    
+}
+.choix{
+  padding: 20px;
+}
+.logo-feedmi{
+  height: 100px;
+  width: 200px;
+}
 #nav a {
   font-weight: bold;
   color: #2c3e50;
