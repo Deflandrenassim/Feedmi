86127c1acdc1ca966b1b425e54ec64e2bf9a190b
Deflandrenassim <nassim.def@gmail.com>
2022-02-26 13:19:11 +0100

    style

--- src/components/Connexion/Login.vue
+++ src/components/Connexion/Login.vue
@@ -1,36 +1,56 @@
 <template>
   <div>
-<h1> hello </h1>
-<form  method="post" class="formulaire ">
-    <div>
-        <label for="name">Nom :</label>
-        <input type="text" id="name" name="user_name">
+    <h1>hello</h1>
+    <div class="container-form">
+      <form class="formulaire">
+        <div>
+          <span> Nom : </span>
+          <label for="name"></label>
+          <input type="text" id="name" name="user_name" />
+        </div>
+        <div>
+          <span> Prénom : </span>
+          <label for="name"></label>
+          <input type="text" id="name" name="user_name" />
+        </div>
+        <div>
+          <span> Email : </span>
+          <label for="mail"></label>
+          <input type="email" id="mail" name="user_mail" />
+        </div>
+        <div>
+          <button class="valider">Validez</button>
+        </div>
+      </form>
     </div>
-     <div>
-        <label for="name">Prénom :</label>
-        <input type="text" id="name" name="user_name">
-    </div>
-    <div>
-        <label for="mail">e-mail:</label>
-        <input type="email" id="mail" name="user_mail">
-    </div>
-   <div>
-     <button> Validez </button>
-   </div>
-</form>
   </div>
 </template>
 
 <script>
 export default {
-    name:'Login'
-}
+  name: "Login",
+};
 </script>
 
 <style>
-.formulaire{
+.container-form{
+  background-color: Grey;
+  height: 500px;
+  width: 500px;
+}
+.formulaire {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.valider {
+  height: 40px;
+  width: 100px;
+  cursor: pointer;
+  color: grey;
+  border-radius: 20px 20px;
+}
+.valider:hover {
+  background-color: rgb(0, 203, 169);
 }
 </style>
