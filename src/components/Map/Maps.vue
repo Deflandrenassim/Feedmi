44d7346026aaea880ab0096207ab6f093d0128ce
Deflandrenassim <nassim.def@gmail.com>
2022-02-28 22:27:23 +0100

    svg color

--- src/components/Map/Maps.vue
+++ src/components/Map/Maps.vue
@@ -9,7 +9,7 @@
      y="0px" viewBox="0 0 1300 550" style="enable-background:new 0 0 1300 550;" xml:space="preserve">
 
 
-    <path class="st1" d="M 0 250 C 600 450 650 100 1300 250 L 1300 550 L 0 550 L 0 250">
+    <path class="st1" fill="#00cba9" fill-opacity="1" d="M 0 250 C 600 450 650 100 1300 250 L 1300 550 L 0 550 L 0 250">
 		<animate
                 attributeName="d"
                 dur="5s"
@@ -21,7 +21,7 @@
         />
 	</path>
 
-	<path class="st1" d="M 0 250 C 600 100 650 450 1300 250 L 1300 550 L 0 550 L 0 250">
+	<path class="st1" fill="#00cba9" fill-opacity="1" d="M 0 250 C 600 100 650 450 1300 250 L 1300 550 L 0 550 L 0 250">
 		<animate
                 attributeName="d"
                 dur="5s"
@@ -32,7 +32,7 @@
         />
 	</path>
 
-	<path class="st1" d="M 0 250 C 950 350 950 150 1300 250 L 1300 550 L 0 550 L 0 250">
+	<path class="st1" fill="#00cba9" fill-opacity="1" d="M 0 250 C 950 350 950 150 1300 250 L 1300 550 L 0 550 L 0 250">
 		<animate
                 attributeName="d"
                 dur="5s"
@@ -44,7 +44,7 @@
         />
 	</path>
 
-	<path class="st1" d="M 0 250 C 350 150 350 350 1300 250 L 1300 550 L 0 550 L 0 250">
+	<path class="st1" fill="#00cba9" fill-opacity="1" d="M 0 250 C 350 150 350 350 1300 250 L 1300 550 L 0 550 L 0 250">
 		<animate
                 attributeName="d"
                 dur="5s"
