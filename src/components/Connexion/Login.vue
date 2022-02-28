2814c57b38f9144d00e6f36878e5260eaf6d720d
Deflandrenassim <nassim.def@gmail.com>
2022-02-28 19:57:44 +0100

    component login add code css

--- src/components/Connexion/Login.vue
+++ src/components/Connexion/Login.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-   
+   <h3 class="animate__animated animate__backInDown"> {{inscrit }} </h3>
     <div class="container-form">
       <form class="formulaire">
         <div class="nom"> 
           <span> Nom : </span>
           <label for="name"></label>
-          <input type="text" id="name" name="user_name" />
+          <input type="text" id="name" name="user_nom" />
         </div>
         <div class="prenom ">
           <span> Prénom : </span>
           <label for="name"></label>
-          <input type="text" id="name" name="user_name" />
+          <input type="text" id="name" name="user_prenom" />
         </div>
         <div class="email">
           <span> Email : </span>
@@ -29,13 +29,23 @@
 <script>
 export default {
   name: "Login",
+  data() {
+    return{
+      inscrit: 'Inscris-toi pour ne rien rater'
+    }
+  }
 };
 </script>
 
 <style>
+h3{
+  display: flex;
+  justify-content: center;
+  font-size: 30px;
+}
 .container-form{
   margin: auto;
-  background-color: Grey;
+  background-color: rgb(219, 218, 218);
   height: 500px;
   width: 500px;
   box-shadow: #03a84e 0 0 16px;
@@ -50,6 +60,7 @@ export default {
 }
 .nom, .email .prenom{
  margin: auto;
+ 
 }
 .valider {
   height: 40px;
@@ -57,6 +68,7 @@ export default {
   cursor: pointer;
   color: rgb(155, 153, 153);
   border-radius: 20px 20px;
+  border: solid 1px black;
 }
 .valider:hover {
   background-color: rgb(0, 203, 169);
