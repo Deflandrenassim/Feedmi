3963fcafbdc3b8796a42f9c5d1b775253d36be28
Deflandrenassim <nassim.def@gmail.com>
2022-03-16 10:17:17 +0100

    add livraison css

--- src/components/Caroussel.vue
+++ src/components/Caroussel.vue
@@ -1,45 +1,69 @@
 <template>
   <div>
-    <carousel
-    :autoplay="true" 
-    :autoplayTimeouy="2000"
-    :mouse-drage="true"
-    >
+    <carousel :autoplay="true" :autoplayTimeouy="2000" :mouse-drage="true">
       <slide>
-      <img src="@/assets/images/carte.png" alt="plat de restaurant"  id="images"/>
+        <img
+          src="@/assets/images/carte.png"
+          alt="plat de restaurant"
+          id="images"
+        />
+        <div class="position">
+          <span class="livraison"> Livraison dans tous le Nord </span>
+        </div>
       </slide>
       <slide>
-      <img src="@/assets/images/plate1.png" alt="plat de restaurant"  id="images" />
+        <img
+          src="@/assets/images/plate1.png"
+          alt="plat de restaurant"
+          id="images"
+        />
       </slide>
       <slide>
-     <img src="@/assets/images/plate2.png" alt="plat de restaurant" id="images" /> 
-     </slide>
-     <slide>
-      <img src="@/assets/images/carte.png" alt="plat de restaurant"  id="images"/>
+        <img
+          src="@/assets/images/plate2.png"
+          alt="plat de restaurant"
+          id="images"
+        />
       </slide>
       <slide>
-      <img src="@/assets/images/plate1.png" alt="plat de restaurant"  id="images"/>
+        <img
+          src="@/assets/images/carte.png"
+          alt="plat de restaurant"
+          id="images"
+        />
+      </slide>
+      <slide>
+        <img
+          src="@/assets/images/plate1.png"
+          alt="plat de restaurant"
+          id="images"
+        />
       </slide>
     </carousel>
   </div>
 </template>
 
 <script>
-import { Carousel, Slide } from 'vue-carousel';
+import { Carousel, Slide } from "vue-carousel";
 export default {
   name: "Caroussel",
   components: {
-      Carousel,
-      Slide
+    Carousel,
+    Slide,
   },
-  
-}
-
+};
 </script>
     
 <style>
-#images{
+#images {
   height: 350px;
   width: 100%;
+}
+.position {
+  color: rgb(0, 203, 169);
+  font-size: 30px;
+  position: absolute;
+  top: 40px;
+  left: 40px;
 }
 </style>
--- src/components/Map/Maps.vue
+++ src/components/Map/Maps.vue
@@ -1,98 +1,114 @@
 <template>
   <div>
- <h4>{{ reseau }} </h4>
+    <h4>{{ reseau }}</h4>
 
-  <div class="slogan">
-   <h1> #La Livraison de maintenant </h1>
-   <i class="fa-solid fa-car-side-bolt"></i>
-  </div>
+    <div class="slogan">
+      <h1>#La Livraison de maintenant</h1>
+      <i class="fa-solid fa-car-bolt"> </i>
+    </div>
 
-<svg class="svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Calque_1" x="0px"
-     y="0px" viewBox="0 0 1300 550" style="enable-background:new 0 0 1300 550;" xml:space="preserve">
-
-
-    <path class="st1" fill="#00cba9" fill-opacity="1" d="M 0 250 C 600 450 650 100 1300 250 L 1300 550 L 0 550 L 0 250">
-		<animate
-                attributeName="d"
-                dur="5s"
-				begin="1s"
-                values="M 0 250 C 600 450 650 100 1300 250 L 1300 550 L 0 550 L 0 250;
+    <svg
+      class="svg"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      id="Calque_1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 1300 550"
+      style="enable-background: new 0 0 1300 550"
+      xml:space="preserve"
+    >
+      <path
+        class="st1"
+        fill="#00cba9"
+        fill-opacity="1"
+        d="M 0 250 C 600 450 650 100 1300 250 L 1300 550 L 0 550 L 0 250"
+      >
+        <animate
+          attributeName="d"
+          dur="5s"
+          begin="1s"
+          values="M 0 250 C 600 450 650 100 1300 250 L 1300 550 L 0 550 L 0 250;
                 M 0 250 C 600 100 650 450 1300 250 L 1300 550 L 0 550 L 0 250;
 				M 0 250 C 600 450 650 100 1300 250 L 1300 550 L 0 550 L 0 250"
-                repeatCount="indefinite"
+          repeatCount="indefinite"
         />
-	</path>
+      </path>
 
-
-	<path class="st1" fill="#00cba9" fill-opacity="1" d="M 0 250 C 600 100 650 450 1300 250 L 1300 550 L 0 550 L 0 250">
-		<animate
-                attributeName="d"
-                dur="5s"
-                values="M 0 250 C 600 100 650 450 1300 250 L 1300 550 L 0 550 L 0 250;
+      <path
+        class="st1"
+        fill="#00cba9"
+        fill-opacity="1"
+        d="M 0 250 C 600 100 650 450 1300 250 L 1300 550 L 0 550 L 0 250"
+      >
+        <animate
+          attributeName="d"
+          dur="5s"
+          values="M 0 250 C 600 100 650 450 1300 250 L 1300 550 L 0 550 L 0 250;
                 M 0 250 C 600 450 650 100 1300 250 L 1300 550 L 0 550 L 0 250;
 				M 0 250 C 600 100 650 450 1300 250 L 1300 550 L 0 550 L 0 250"
-                repeatCount="indefinite"
+          repeatCount="indefinite"
         />
-	</path>
+      </path>
 
-	<path class="st1" fill="#00cba9" fill-opacity="1" d="M 0 250 C 950 350 950 150 1300 250 L 1300 550 L 0 550 L 0 250">
-		<animate
-                attributeName="d"
-                dur="5s"
-				begin="2s"
-                values="M 0 250 C 950 350 950 150 1300 250 L 1300 550 L 0 550 L 0 250;
+      <path
+        class="st1"
+        fill="#00cba9"
+        fill-opacity="1"
+        d="M 0 250 C 950 350 950 150 1300 250 L 1300 550 L 0 550 L 0 250"
+      >
+        <animate
+          attributeName="d"
+          dur="5s"
+          begin="2s"
+          values="M 0 250 C 950 350 950 150 1300 250 L 1300 550 L 0 550 L 0 250;
                 M 0 250 C 350 150 350 350 1300 250 L 1300 550 L 0 550 L 0 250;
 				M 0 250 C 950 350 950 150 1300 250 L 1300 550 L 0 550 L 0 250"
-                repeatCount="indefinite"
+          repeatCount="indefinite"
         />
-	</path>
+      </path>
 
-	<path class="st1" fill="#00cba9" fill-opacity="1" d="M 0 250 C 350 150 350 350 1300 250 L 1300 550 L 0 550 L 0 250">
-		<animate
-                attributeName="d"
-                dur="5s"
-                values="M 0 250 C 350 150 350 350 1300 250 L 1300 550 L 0 550 L 0 250;
+      <path
+        class="st1"
+        fill="#00cba9"
+        fill-opacity="1"
+        d="M 0 250 C 350 150 350 350 1300 250 L 1300 550 L 0 550 L 0 250"
+      >
+        <animate
+          attributeName="d"
+          dur="5s"
+          values="M 0 250 C 350 150 350 350 1300 250 L 1300 550 L 0 550 L 0 250;
                 M 0 250 C 950 350 950 150 1300 250 L 1300 550 L 0 550 L 0 250;
 				M 0 250 C 350 150 350 350 1300 250 L 1300 550 L 0 550 L 0 250"
-                repeatCount="indefinite"
+          repeatCount="indefinite"
         />
-	</path>
-
-</svg>
-
-
-
+      </path>
+    </svg>
   </div>
 </template>
 
 <script>
 export default {
-    name:'Maps'
-}
+  name: "Maps",
+};
 </script>
 
 <style scoped>
-.slogan{
-  display:flex;
+.slogan {
+  display: flex;
   justify-content: center;
   padding-top: 10px;
 }
-.container-logo{
+.container-logo {
   display: flex;
   flex-direction: column;
   justify-content: center;
-
 }
-.logo{
+.logo {
   display: flex;
   justify-content: space-around;
-position: absolute;
-    bottom : -100px;
+  position: absolute;
+  bottom: -100px;
 }
-
-
-
-
-
-
 </style>
